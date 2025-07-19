c749b65da037bb58414aa3023040426a48b25f88
damenta-yg <gnitnigdy@outlook.com>
2025-07-19 19:29:11 +0700

    Finishing Section 8

---
+++ 08-00-BooleanAndConditionalLogic.py
@@ -0,0 +1,47 @@
+#conditionals if
+"""
+if x == 2:
+    print('a')
+elif x == 3:
+    print('b')
+elif x == 4:
+    print('d')    
+else:
+    print('c')
+"""
+
+#truthy values
+"""
+animals = 0 -> falsy value
+x is 1 ->truthy
+x is 0 -> falsy
+"""
+
+
+#comparison operator
+"""
+if age >= 7
+if age <= 7
+"""
+
+#logical operator (AND,OR, NOT)
+"""
+if age >= 7 AND age <=8
+and -> two statements must be true
+or -> need only one statement to be true
+"""
+
+#is vs ==
+"""
+in python == and is are very similar comparators but not same
+example:
+a = 1
+a == 1 #value based store
+a is 1
+
+a = [1,2,3]
+b = [1,2,3]
+
+a == b -> True
+a is b -> False
+"""
---
+++ 08-06-CodeAlong-NestedConditionals.py
@@ -0,0 +1,4 @@
+age = int(input("How old are you ?\n"))
+age2 = input("How old are you ?\n")
+print(type(age).__name__)
+print(type(age2).__name__)
